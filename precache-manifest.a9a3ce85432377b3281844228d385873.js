6f5f875177a6e8fc2ae8596ab3a45ac956fba81c
M. Oguzhan Ataman <moguzhanataman@gmail.com>
2018-11-20 00:22:08 +0300

    Updates

---
+++ precache-manifest.a9a3ce85432377b3281844228d385873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d98853326d7dc6aff4d",
+    "url": "/math-data-demo/static/js/runtime~main.3d988533.js"
+  },
+  {
+    "revision": "11826498aae00cf1a897",
+    "url": "/math-data-demo/static/js/main.11826498.chunk.js"
+  },
+  {
+    "revision": "548329c8b28fce91368c",
+    "url": "/math-data-demo/static/js/1.548329c8.chunk.js"
+  },
+  {
+    "revision": "11826498aae00cf1a897",
+    "url": "/math-data-demo/static/css/main.75302f61.chunk.css"
+  },
+  {
+    "revision": "9635ef4fe8c40321661691237b96181b",
+    "url": "/math-data-demo/index.html"
+  }
+];
